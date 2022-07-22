03d8b0caf2c55d20137e9bbef80fb0928c01455e
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-07-21 22:35:12 -0300

    added mobile menu

--- script/app.js
+++ script/app.js
@@ -1,5 +1,14 @@
 
 window.onload = () =>{
+    const button = document.querySelector("#mobileMenu")
+
+    typingAnimation()
+
+    button.addEventListener("click", activeMenu)
+
+}
+
+function typingAnimation(){
     const textMain = document.querySelector('.textMain')
 
     const name = "Gabriel_Waltmann"
@@ -11,6 +20,7 @@ window.onload = () =>{
     setInterval(() => {
         document.querySelector("#line").classList.toggle('notVisible');
     }, 300);
+
     setTimeout(() => {
         setInterval(() => {
             if(currentLetter < numberOfLetters){
@@ -20,7 +30,25 @@ window.onload = () =>{
     
         }, 300);
     }, 1500);
-    
+}
+
+function activeMenu(){
+    const menu = document.querySelector(".menu")
+
+    menu.classList.toggle('active');
+}
+
+function animeScroll() {
+	
+	document.querySelectorAll('[data-anime]').forEach((element, index) => {
+		if(element.getBoundingClientRect().top < window.innerHeight) {
+			element = element.setAttribute('class', "active");
+		}else{
+            element = element.removeAttribute('class', "active");
+        }		
+	})
 
 }
 
+
+window.addEventListener('scroll', animeScroll);
