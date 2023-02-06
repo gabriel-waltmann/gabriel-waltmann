d73d390c108e977601d479f5a595f55a8981e6fb
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-02-06 13:39:45 -0300

    Perf: Clear public folder

--- src/components/variables.ts
+++ src/components/variables.ts
@@ -2,3 +2,4 @@ import { purple, red, teal } from '@mui/material/colors';
 
 // coolrs
 export const primary = teal['A700']; // #00e676
+export const secundary = teal[500]; // #009688
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { createTheme } from '@mui/material';
 import type { AppProps } from 'next/app'
+import 'styles/global.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
