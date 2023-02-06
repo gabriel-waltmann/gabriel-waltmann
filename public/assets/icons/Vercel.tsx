04a44ff75d81b7d443a750a0fb9e8f1bbae42879
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-02-06 17:59:12 -0300

    Style: Create techs Icons

---
+++ public/assets/icons/Vercel.tsx
@@ -0,0 +1,12 @@
+export default function Vercel() {
+  return (
+        <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="ionicon"
+      viewBox="0 0 512 512"
+    >
+      <path fillRule="evenodd" d="M256 48l240 416H16z"></path>
+    </svg>
+  );
+}
+
