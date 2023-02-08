ae328b342318be31b58837eec71b2498511fed64
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-02-08 19:54:07 -0300

    Style: Change Logo color

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Logo(){
     return(
-        <Typography variant="h6" sx={{ my: 2 }}>
+        <Typography variant="h6" sx={{ my: 2, color: '#ffff' }} id='logo'>
         &lt;Gabriel	&frasl;&gt;
       </Typography>
     )
