73db8a4bb4d2b80d40fab055b61b8bbb423d41c4
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-02-10 16:40:21 -0300

    Build: fix import about.scss

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: ['media.graphassets.com'],
   },
   sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+        includePaths: [path.join(__dirname, './src/styles')],
         prependData: `@import "./src/styles/global.scss";`,
     },
 
