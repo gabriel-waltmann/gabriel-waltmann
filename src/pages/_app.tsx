eaad96db241a499f38db7b74668abf0140b4382f
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2023-02-11 16:01:04 -0300

    SEO: add keyword meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,9 @@
 import type { AppProps } from 'next/app'
 import '../styles/global.scss'
-import { ApolloProvider } from '@apollo/client'
-import { client } from 'apolloClient'
-import { DefaultSeo } from 'next-seo';
-import SEO from '../../next-seo.config';
-import { useEffect } from 'react'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  useEffect(()=>{
-    let element: HTMLElement | null = document.querySelector('html')
-    if(element) element.setAttribute('lang', "pt-BR")
-  },[])
   return (
-
-      <ApolloProvider client={client}>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-      </ApolloProvider>
-
-
+    <Component {...pageProps} />
   )
 }
