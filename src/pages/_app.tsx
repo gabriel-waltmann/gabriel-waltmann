0e34d35c52340a6768da7b7bf81b0086fc647ed3
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-12-26 07:16:15 -0300

    fix: correct just font import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,10 @@
 import type { AppProps } from 'next/app'
 import '../styles/global.scss'
 
-import { createTheme, Theme, ThemeProvider } from "@mui/material/styles";
-import { Jost } from '@next/font/google';
-
-const jost = Jost({
-  weight: ["300", "400"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-});
-
-const theme: Theme = createTheme({
-  typography: {
-    fontSize: 16,
-    fontFamily: jost.style.fontFamily,
-  }
-})
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <Component {...pageProps} />
-      </div>
-    </ThemeProvider>
+    <div>
+      <Component {...pageProps} />
+    </div>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { DefaultSeo } from 'next-seo';
-import SEO from '../../next-seo.config';
 
 export default function Document() {
 
     return (
         <Html lang='pt_BR'>
             <Head >
+                <title>Gabriel Waltmann</title>
+                <meta name="description" content="Gabriel Waltmann Portifolio" />
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
                 <meta name="keywords" content="Typescript, Fullstack, JavaScript, Front-End, Gabriel Waltmann, Portfólio, Portfólio Gabriel Waltmann"></meta>
             </Head>
             <body>
