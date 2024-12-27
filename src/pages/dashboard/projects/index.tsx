332ddc398013d0d9e3a08f6ae2fdc646bbfae61a
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-12-27 09:51:03 -0300

    feat: add dashboard project techs

--- src/pages/dashboard/projects/index.tsx
+++ src/pages/dashboard/projects/index.tsx
@@ -3,10 +3,12 @@ import DashboardProjectCard from "@/components/dashboard/dashboard-project"
 import TypographyTitle from "@/components/typography/title"
 import { ProjectEntity } from "@/entities/project/ProjectEntity"
 import { ProjectFileEntity } from "@/entities/project/ProjectFiles"
+import { ProjectTechEntity } from "@/entities/project/ProjectTechEntity"
+import { TechEntity } from "@/entities/TechEntity"
 import DashboardLayout from "@/layouts/dashboard"
 import { DevTool } from "@hookform/devtools"
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, ImageList, ImageListItem, ImageListItemBar, Tab, Tabs, TextField } from "@mui/material"
-import { Trash } from "@phosphor-icons/react"
+import { Box, Button, Chip, Dialog, DialogActions, DialogContent, IconButton, ImageList, ImageListItem, ImageListItemBar, Tab, Tabs, TextField, Typography } from "@mui/material"
+import { Plus, Trash, X } from "@phosphor-icons/react"
 import { CSSProperties, useEffect, useState } from "react"
 import { Controller, useForm } from "react-hook-form"
 
@@ -24,6 +26,7 @@ type TFormFields = {
 enum TabsEnum {
   General = 0,
   Files = 1,
+  Techs = 2,
 }
 function a11yProps(index: TabsEnum) {
   return {
@@ -48,13 +51,23 @@ export default function Projects() {
   const [saveLoading, setSaveLoading] = useState<boolean>(false);
   const [deleteLoading, setDeleteLoading] = useState<boolean>(false);
   const [projects, setProjects] = useState<ProjectEntity[]>([]);
+  const [techs, setTechs] = useState<TechEntity[]>([]);
   const [projectFiles, setProjectFiles] = useState<ProjectFileEntity[]>([]);
+  const [projectTechs, setProjectTechs] = useState<ProjectTechEntity[]>([]);
+  const [tab, setTab] = useState<TabsEnum>(TabsEnum.General);
   
   const handleProjects = async () => {
     const response = await fetch('/api/project');
     const result: ProjectEntity[] = await response.json();
 
     setProjects(result);
+  }
+
+  const handleTechs = async () => {
+    const response = await fetch('/api/tech');
+    const result: TechEntity[] = await response.json();
+
+    setTechs(result);
   }
 
   useEffect(() => {
@@ -167,8 +180,6 @@ export default function Projects() {
     setActiveDialog(false)
   }
 
-  const [tab, setTab] = useState<TabsEnum>(TabsEnum.General);
-
   const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
     setTab(newValue as TabsEnum);
   };
@@ -181,12 +192,30 @@ export default function Projects() {
     setProjectFiles(result);
   }
 
+  const handleProjectTechs = async (projectId: string) => {
+    const response = await fetch(`/api/project/${projectId}/tech`);
+
+    const result: ProjectTechEntity[] = await response.json();
+
+    setProjectTechs(result);
+  }
+
   useEffect(() => {
     const projectId = form.watch("id");
+    
     if (activeDialog && tab === TabsEnum.Files && projectId) {
       handleProjectFiles(projectId);
-    } else {
+    }
+    
+    else if (activeDialog && tab === TabsEnum.Techs && projectId) {
+      handleTechs();
+      handleProjectTechs(projectId);
+    }
+
+    else {
       setProjectFiles([]);
+      setProjectTechs([]);
+      setTechs([]);
     }
   }, [tab])
 
@@ -196,11 +225,55 @@ export default function Projects() {
     });
 
     if (response.status !== 200) {
-      return alert("Erro ao cadastrar tech");
+      return alert("Erro ao deletar file");
     }
 
     const newProjectFiles = projectFiles.filter(({ id }) => id !== projectFile.id);
     setProjectFiles(newProjectFiles);
+  }
+
+  const handleDeleteProjectTech = async (projectTech: ProjectTechEntity) => {
+    const response = await fetch(`/api/project/${projectTech.project.id}/tech/${projectTech.id}`, {
+      method: "DELETE",
+    });
+
+    if (response.status !== 200) {
+      return alert("Erro ao deletar tech");
+    }
+
+    const newProjectTechs = projectTechs.filter(({ id }) => id !== projectTech.id);
+    setProjectTechs(newProjectTechs);
+  }
+
+  const handleAddProjectTech = async (tech: TechEntity) => {
+    const projectId = form.watch("id");
+
+    if (!projectId?.length) {
+      return alert("Erro ao incluir tech");
+    }
+
+    const formData = new FormData();
+    formData.append("techId", tech.id);
+
+    const response = await fetch(`/api/project/${projectId}/tech`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.status !== 200) {
+      return alert("Erro ao incluir tech");
+    }
+
+    const result: ProjectTechEntity = await response.json();
+
+    const newProjectTechs = [...projectTechs];
+    newProjectTechs.unshift(result);
+    
+    setProjectTechs(newProjectTechs);
+  }
+
+  const techAlreadyExists = (tech: TechEntity) => {
+    return projectTechs.some((projectTech) => projectTech.tech.id === tech.id);
   }
 
   return (
@@ -220,7 +293,7 @@ export default function Projects() {
         margin: "1rem auto"
       }}>
         {projects.map(project =>
-        <li key={project.id}>
+        <li key={project.id+"project"}>
           <DashboardProjectCard
             project={project} 
             onClick={() => onEditProject(project)}
@@ -233,88 +306,112 @@ export default function Projects() {
         open={activeDialog}
         onClose={() => setActiveDialog(false)}
         onSubmit={handleSubmit((data) => onSubmit(data))}
-        PaperProps={{ component: 'form' }}
+        PaperProps={{ component: 'form' }} 
         
       >
-        <DialogTitle>Project</DialogTitle>
-        <DialogContent sx={{ width: "600px", height: "60vh" }}>
+        <IconButton
+          aria-label="close"
+          onClick={() => setActiveDialog(false)}
+          sx={(theme) => ({
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+            cursor: 'pointer'
+          })}
+        >
+          <X />
+        </IconButton>
+        <DialogContent sx={{ width: "600px", height: "600px", display: "flex", flexDirection: "column" }}>
           <Tabs value={tab} onChange={handleChangeTab} aria-label="Project tabs">
-            <Tab label="Geral" {...a11yProps(TabsEnum.General)} />
-            <Tab label="Arquivos" {...a11yProps(TabsEnum.Files)} />
+            <Tab label="General" {...a11yProps(TabsEnum.General)} />
+            <Tab label="Files" {...a11yProps(TabsEnum.Files)} />
+            <Tab label="Techs" {...a11yProps(TabsEnum.Techs)} />
           </Tabs>
 
-          <Box sx={{marginTop: "1rem"}}>
+          <Box sx={{marginTop: "1rem", height: "100%", display: "flex", flexDirection: "column", gap: "1rem"}}>
             {tab === TabsEnum.General && <>
-              <Controller
-                control={control}
-                name="title"
-                render={({
-                  field: { onChange, onBlur, value, name, ref },
-                  fieldState: { invalid, isTouched, isDirty, error },
-                  formState,
-                }) => (
-                  <TextField
-                    autoFocus
-                    required
-                    margin="dense"
-                    id="title"
-                    label="Title"
-                    type="text"
-                    fullWidth
-                    variant="outlined"
-                    {...register("title", {
-                      validate: value => value.length >= 3 || "Campo obrigatório",
-                      onChange: () => form.trigger("title")
-                    })}
-                    value={value}
-                    onChange={onChange}
-                    error={!!error}
-                    helperText={error ? error.message : null}
-                  />
-                )}
-              />
+              <div style={{ flex: 1}}>
+                <Controller
+                  control={control}
+                  name="title"
+                  render={({
+                    field: { onChange, onBlur, value, name, ref },
+                    fieldState: { invalid, isTouched, isDirty, error },
+                    formState,
+                  }) => (
+                    <TextField
+                      autoFocus
+                      required
+                      margin="dense"
+                      id="title"
+                      label="Title"
+                      type="text"
+                      fullWidth
+                      variant="outlined"
+                      {...register("title", {
+                        validate: value => value.length >= 3 || "Campo obrigatório",
+                        onChange: () => form.trigger("title")
+                      })}
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                    />
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name="description"
+                  render={({
+                    field: { onChange, onBlur, value, name, ref },
+                    fieldState: { invalid, isTouched, isDirty, error },
+                    formState,
+                  }) => (
+                    <TextField
+                      sx={{ width: "100%" }}
+                      minRows={6}
+                      multiline
+                      size="medium"
+                      autoFocus
+                      required
+                      margin="dense"
+                      id="description"
+                      label="Description"
+                      type="text"
+                      fullWidth
+                      variant="outlined"
+                      {...register("description", {
+                        validate: value => {
+                          if (value.length < 20) return "Campo requer mínimo de 20 caracteres";
+                          return true;
+                        },
+                        onChange: () => form.trigger("description")
+                      })}
+                      value={value}
+                      onChange={onChange}
+                      error={!!errors.description}
+                      helperText={errors.description?.message}
+                    />
+                  )}
+                />
+              </div>
 
-              <Controller
-                control={control}
-                name="description"
-                render={({
-                  field: { onChange, onBlur, value, name, ref },
-                  fieldState: { invalid, isTouched, isDirty, error },
-                  formState,
-                }) => (
-                  <TextField
-                    sx={{ width: "100%" }}
-                    minRows={6}
-                    multiline
-                    size="medium"
-                    autoFocus
-                    required
-                    margin="dense"
-                    id="description"
-                    label="Description"
-                    type="text"
-                    fullWidth
-                    variant="outlined"
-                    {...register("description", {
-                      validate: value => {
-                        if (value.length < 20) return "Campo requer mínimo de 20 caracteres";
-                        return true;
-                      },
-                      onChange: () => form.trigger("description")
-                    })}
-                    value={value}
-                    onChange={onChange}
-                    error={!!errors.description}
-                    helperText={errors.description?.message}
-                  />
-                )}
-              />
+              <DialogActions style={{ gap: ".6rem" }}>
+                {form.watch("id") && <Button onClick={() => onDeleteProject()} variant="contained">
+                  {deleteLoading ? "DELETANDO..."  : "DELETAR"}
+                </Button>}
+
+                <Button variant="contained" type="submit">
+                  {saveLoading ? "CARREGANDO..." : form.watch("id")?.length ? "ALTERAR" : "CADASTRAR"}
+                </Button>
+              </DialogActions>
             </>}
 
-            {tab === TabsEnum.Files && 
-              <ImageList gap={16} cols={1} sx={{ marginTop: "1rem", width: "100%", paddingRight: "1rem", paddingLeft: "1rem" }}>
+            {tab === TabsEnum.Files && <div>
+              <ImageList gap={16} cols={1} sx={{ marginTop: "1rem", marginBottom: "1rem", width: "100%", paddingRight: "1rem", paddingLeft: "1rem" }}>
                 {projectFiles.map(projectFile => (
-                  <ImageListItem key={projectFile.id}>
+                  <ImageListItem key={projectFile.id+"projectFile"}>
                     <img
                       src={projectFile.file.key}
                       alt={projectFile.file.name}
@@ -381,23 +478,47 @@ export default function Projects() {
                   </Button>
                 </ImageListItem>
               </ImageList>
-            }
+              </div>}
+
+            {tab === TabsEnum.Techs && <>
+              <section>
+                <Typography variant="body1" sx={{ fontWeight: "bold", color: 'text.secondary' }}>
+                  Techs do projeto
+                </Typography>
+                <ul style={{ display: "flex", flexWrap: "wrap", gap: ".6rem" }}>
+                  {projectTechs.map(projectTech => (
+                    <li key={projectTech.id+"projectTech"}>
+                      <Chip
+                        label={projectTech.tech.name}
+                        variant="outlined"
+                        deleteIcon={<Trash size={16} />}
+                        onDelete={() => handleDeleteProjectTech(projectTech)}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </section>
+
+              <section>
+                <Typography variant="body1" sx={{ fontWeight: "bold", color: 'text.secondary' }}>
+                  Techs disponiveis
+                </Typography>
+                <ul style={{ display: "flex", flexWrap: "wrap", gap: ".6rem" }}>
+                  {techs.filter(tech => !techAlreadyExists(tech)).map(tech =>
+                    <li key={tech.id+"tech"}>
+                      <Chip
+                        label={tech.name}
+                        variant="outlined"
+                        deleteIcon={<Plus size={16} />}
+                        onDelete={() => handleAddProjectTech(tech)}
+                      />
+                    </li>
+                  )}
+                </ul>
+              </section>
+            </>}
           </Box>
-
         </DialogContent>
-
-        <DialogActions style={{ gap: ".6rem" }}>
-          <Button variant="outlined" onClick={() => setActiveDialog(false)}>CANCELAR</Button>
-
-          {form.watch("id") && <Button onClick={() => onDeleteProject()} variant="contained">
-            {deleteLoading ? "DELETANDO..."  : "DELETAR"}
-          </Button>}
-
-          <Button variant="contained" type="submit">
-            {saveLoading ? "CARREGANDO..." : form.watch("id")?.length ? "ALTERAR" : "CADASTRAR"}
-          </Button>
-        </DialogActions>
-        
         <DevTool control={control} />
       </Dialog>
     </DashboardLayout>
