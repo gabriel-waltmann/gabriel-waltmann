49bf169d40dd949d735eaad706ceccfbc07f3f02
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-12-30 06:16:17 -0300

    build: clean index page

--- src/components/Main/Projects/index.tsx
+++
@@ -1,9 +0,0 @@
-import List from './List';
-
-export default function Projects() {
-  return (
-    <section id='projects'>
-      <List />
-    </section>
-  )
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,6 @@
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
-import Head from 'next/head'
-
 export default function Home({ data }: any) {
   return (
-
     <>
-      <Head>
-        <title>Portfólio Gabriel Waltmann</title>
-        <meta name="description" content="Desnvolvedor Front End " />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/assets/favicon.ico" />
-      </Head>
-
-      <>
-        <Header />
-
-        <Main />
-
-        <Footer />
-      </>
     </>
   )
 }
